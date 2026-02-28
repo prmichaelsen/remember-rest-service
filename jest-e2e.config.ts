ce028db74292fe0a427fe3a57283c81d8b3bed74
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2026-02-28 14:53:52 +0000

    feat: tsconfig fixes

---
+++ jest-e2e.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.e2e\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
+
+export default config;
